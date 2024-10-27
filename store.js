9b2451766d61b797cef268aca06d5e6931f47f71
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2024-10-28 00:28:45 +0100

    Adv. Redux second commit

--- store.js
+++ store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import userReducer from "./src/features/user/userSlice";
+import cartReducer from "./src/features/cart/cartSlice";
 
 const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: { user: userReducer, cart: cartReducer },
 });
 
 export default store;
